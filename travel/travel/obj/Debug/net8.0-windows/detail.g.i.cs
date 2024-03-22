6f7f7b24b05ac65f32db4c0a247b9e44bba9cdd4
quang <huuquang1511@gmail.com>
2024-03-22 12:02:04 +0700

    toi uu code

--- travel/travel/obj/Debug/net8.0-windows/detail.g.i.cs
+++ travel/travel/obj/Debug/net8.0-windows/detail.g.i.cs
@@ -53,7 +53,7 @@ namespace travel {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/travel;component/detail.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/travel;V1.0.0.0;component/detail.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\detail.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
