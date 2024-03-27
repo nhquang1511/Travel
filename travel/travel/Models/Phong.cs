81c3b521d29264233109a11c941702b4195ddc86
quang <huuquang1511@gmail.com>
2024-03-27 12:42:11 +0700

    s

---
+++ travel/travel/Models/Phong.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travel.Models
+{
+    public class Phong
+    {
+        public int PhongID { get; set; }
+        public string TenPhong { get; set; }
+        public int KhachSanID { get; set; }
+        public string TenLoaiPhong { get; set; }
+        public string TrangThai { get; set; }
+
+        public Phong(int PhongID, string TenPhong, int KhachSanID, string TenLoaiPhong, string TrangThai)
+        {
+            this.PhongID = PhongID;
+            this.TenPhong = TenPhong;
+            this.KhachSanID = KhachSanID;
+            this.TenLoaiPhong = TenLoaiPhong;
+            this.TrangThai = TrangThai;
+        }
+    }
+}
